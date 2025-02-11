ac2af16855935a3aceb741dc1daafee52372a5f9
Scott Offen (Tech9) <118468886+sdoffen@users.noreply.github.com>
2025-02-11 07:10:01 -0700

    Add Extension Unit Tests for Stand-Alone Mapper (#14)

---
+++ src/content/template-layer-mapper/stand-alone/Template9.Default.Tests/Extensions/TestMapperDestination.cs
@@ -0,0 +1,12 @@
+namespace Template9.Default.Tests.Extensions;
+
+public class TestMapperDestination
+{
+    public string IgnoreProperty { get; set; } = null!;
+    public int IntPropertyDest { get; set; }
+    public double DoublePropertyDest { get; set; }
+    public float FloatPropertyDest { get; set; }
+    public string StringPropertyDest { get; set; } = null!;
+    public Guid GuidProperty { get; set; }
+    public int SomeNumber { get; set; }
+}
