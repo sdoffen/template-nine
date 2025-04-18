8be04a24d14a5a2b9261f865fd65e7a7ae857520
Scott Offen <118468886+sdoffen@users.noreply.github.com>
2025-04-18 16:05:03 -0600

    Add generic configurations

--- src/content/template-layer-efcore/Template9.Design/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Template9.Design;
-
-public class Class1
-{
-
-}
--- src/content/template-layer-efcore/Template9/CompositionExtensions.cs
+++ src/content/template-layer-efcore/Template9/CompositionExtensions.cs
@@ -1,6 +1,36 @@
-﻿namespace Template9;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Template9;
 
 public static class CompositionExtensions
 {
+    public static IServiceCollection RegisterDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var databaseOptions = configuration.GetSection(DatabaseOptions.SectionName).Get<DatabaseOptions>()
+            ?? throw new InvalidOperationException($"Missing {DatabaseOptions.SectionName} section in configuration.");
 
+        // var optionsAction = GetDbContextOptionsAction(databaseOptions);
+
+        if (databaseOptions.UseDbContextPooling)
+        {
+            // services.AddDbContextPool<ProjectContext>(optionsAction);
+        }
+        else
+        {
+            // services.AddDbContext<ProjectContext>(optionsAction);
+        }
+
+        return services;
+    }
+
+    // private static Action<DbContextOptionsBuilder> GetDbContextOptionsAction(DatabaseOptions options)
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    // private static string GetConnectionString(DatabaseOptions options)
+    // {
+    //     throw new NotImplementedException();
+    // }
 }
